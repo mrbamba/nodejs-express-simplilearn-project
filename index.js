98adc2496bd4455970d29e6fe533b803aadd96bc
Dan Haski <dhaski@gmail.com>
2023-01-01 10:36:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express/urlencoded({extended:false}));
+app.use("/api/users", require("./routes/api/users"));
+
+app.get('/', (req,res)=> {
+    res.send('What you looking at willis!');
+})
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000');
+})
